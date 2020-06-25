fd69e7033b353ccda43735b13ba56927b7992b9a
GreenApple131 <dimalg400@gmail.com>
2020-06-25 23:27:50 +0300

    Redux Thunk

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,8 +1,24 @@
-import { DISHES } from '../shared/dishes'; // дані про страви(назва, ціна, опис...)
+import * as ActionTypes from './ActionTypes';
 
 
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (state = {
+	isLoading: true,
+	errMess: null,
+	dishes:[]
+}, action) => {
 	switch(action.type) {
+		case ActionTypes.ADD_DISHES:
+			return {...state, isLoading: false, errMess: null, dishes: action.payload}  // береться об'єкт state і ту, що знаходиться після коми модифікує його і повертає цей содифікований об'єкт
+
+
+		case ActionTypes.DISHES_LOADING:
+			return {...state, isLoading: true, errMess: null, dishes: []}  // береться об'єкт state і ту, що знаходиться після коми модифікує його і повертає цей содифікований об'єкт
+
+		case ActionTypes.DISHES_FAILED:
+			return {...state, isLoading: false, errMess: action.payload, dishes: []}  // береться об'єкт state і ту, що знаходиться після коми модифікує його і повертає цей содифікований об'єкт
+
+
+
 		default:
 			return state;
 	}
